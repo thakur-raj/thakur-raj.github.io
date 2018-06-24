1562914ccd014751a5f857a6d28d32f7715f00f7
rajthakur1 <rajthakur2003@gmail.com>
2018-06-24 21:58:19 +0530

    After creating chats,status,calls

--- whatsapp/script.js
+++ whatsapp/script.js
@@ -2,6 +2,7 @@ $(window).on('resize', function () {
     var win = $(this);
     if (win.width() > 767) {
                $('#moreOptions').addClass('dropdown-menu-right');
+      
     }
     else {
         $('#moreOptions').removeClass('dropdown-menu-right');
